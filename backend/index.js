a305a554861cfb061ea860103decb0a2569e7392
KiranRivera <rivera.emanuel.cbtis37@gmail.com>
2026-04-27 14:23:54 -0700

    conexión con el backend

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173", 
-    "https://pro-active-beta.vercel.app" 
+    "https://pro-active-beta.vercel.app/" 
   ],
   methods: ["GET", "POST", "PATCH", "DELETE"],
   credentials: true
