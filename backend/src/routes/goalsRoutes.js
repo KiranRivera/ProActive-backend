193916917143da77d1bd0e207e529698189e2f71
KiranRivera <rivera.emanuel.cbtis37@gmail.com>
2026-04-27 21:05:19 -0700

    correccion de rutas adicional

--- backend/src/routes/goalsRoutes.js
+++ backend/src/routes/goalsRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const goalsController = require('../controllers/premium/goalsController');
 
 // Rutas de Metas Premium (Añadimos /goals a cada una)
-router.get('/goals/:userId', goalsController.getGoals);
-router.post('/goals/:userId', goalsController.createGoal);
-router.patch('/goals/:userId/:goalId', goalsController.updateGoalProgress);
-router.delete('/goals/:userId/:goalId', goalsController.deleteGoal);
+router.get('/:userId', goalsController.getGoals);
+router.post('/:userId', goalsController.createGoal);
+router.patch('/:userId/:goalId', goalsController.updateGoalProgress);
+router.delete('/:userId/:goalId', goalsController.deleteGoal);
 
 module.exports = router;
