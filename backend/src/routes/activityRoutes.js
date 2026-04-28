73d2f780af0bedb12eb3021e9bad9daee790ac9c
KiranRivera <rivera.emanuel.cbtis37@gmail.com>
2026-04-27 21:09:12 -0700

    correccion de rutas final

--- backend/src/routes/activityRoutes.js
+++ backend/src/routes/activityRoutes.js
@@ -5,16 +5,16 @@ const activityController = require('../controllers/bussiness/activityController'
 
 // 1. LAS RUTAS MÁS ESPECÍFICAS SIEMPRE VAN PRIMERO
 // URL: GET /api/business/activities/teams/:userId/:teamId/members
-router.get('/activities/teams/:userId/:teamId/members', activityController.getTeamMembers);
+router.get('/teams/:userId/:teamId/members', activityController.getTeamMembers);
 
 // 2. RUTAS DE ACTIVIDADES
 // URL: GET /api/business/activities/:userId/:teamId
-router.get('/activities/:userId/:teamId', activityController.getTeamActivities);
+router.get('/:userId/:teamId', activityController.getTeamActivities);
 
 // URL: POST /api/business/activities/:userId/:teamId
-router.post('/activities/:userId/:teamId', activityController.createActivityByEmail);
+router.post('/:userId/:teamId', activityController.createActivityByEmail);
 
 // URL: PATCH /api/business/activities/:userId/:teamId/:activityId
-router.patch('/activities/:userId/:teamId/:activityId', activityController.updateActivityStatus);
+router.patch('/:userId/:teamId/:activityId', activityController.updateActivityStatus);
 
 module.exports = router;
